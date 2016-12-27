75c32da9561213c278021e141c61c9cab79e060b
niskase <niskanenpetri1@gmail.com>
2016-12-27 19:17:42 +0200

    user.java

--- src/main/java/nykkis/User.java
+++ src/main/java/nykkis/User.java
@@ -1,6 +1,5 @@
 package nykkis;
 
-<<<<<<< HEAD
 import java.io.Serializable;
 import java.util.List;
 
@@ -78,52 +77,4 @@ public class User implements Serializable {
 				+ ", loginstatus=" + loginstatus + "]";
 	}
 	
-	
-	
-
-=======
-import java.util.ArrayList;
-import java.util.List;
-
-public class User {
-	private static enum Roles {admin, seller, boss};
-	
-	private int user_id;
-	private String user_name;
-	private String passwd;
-	private int login_status; 
-	private Roles role;
-	
-	
-	
-	public void addCustomer(Customer cus){
-	//	if(role == Roles.admin || role == Roles.seller)
-			
-			ArrayList<Customer> customers = new ArrayList();
-			
-		
-	}
-	
-	public void removeCustomer(){
-		
-		
-	}
-	
-	public void addCompany(){
-		
-	}
-	
-	public void removeCompany(){
-		
-	}
-	
-	public void sentMessageToAdmin(){
-		
-	}
-	
-	public void setCustomerStatus(){
-		
-	}
->>>>>>> c3686833434fb0b451d652ba8d55c5f07faf4dcd
-
 }
